b09f96cf1c828b003a48c44b6644cf1c42daebd3
nurova <76431199+nurova@users.noreply.github.com>
2020-12-21 15:48:50 +0500

    Add files via upload
    Lesson 1

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    // Example 1
+    // int a, b, c = 6;
+    // a = 5;
+    // cout << a + c;
+
+    // Example 2
+    // int a = 10;
+    // int b = 5;
+    // cout << a + b << endl;
+    // cout << a - b << endl;
+    // cout << a * b << endl;
+    // cout << a / b << endl;
+    // cout << a % b << endl;
+    // cout << (a % b == 0) << endl;
+
+    // Example 3
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
